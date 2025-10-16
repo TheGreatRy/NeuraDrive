976e9a3ae407cae9415c7930e758d0904ab6884c
Lace-Gang <rhall@student.neumont.edu>
2025-10-15 18:19:52 -0600

    Buttons and Entries Push
    Buttons and Text Entries in each page

--- RacingPage.xaml.cs
+++ RacingPage.xaml.cs
@@ -6,4 +6,16 @@ public partial class RacingPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    //The following is here until the second page is created
+    private void OnBetClicked(object sender, EventArgs e)
+    {
+        //Places a bet on a racer
+        //Has to validate that the user has enough funds to place their bet
+        //Updates their funds/winnings to reflect the bet (takes out whatever they bet)
+        //Updates the car object with the bet
+
+        //Some (or probably all) of the functionality of this method may be established in other methods
+    }
 }
