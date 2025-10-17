a6c070eb1c9959a9e742ed604a1070419cd0dcd6
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2025-10-16 19:18:53 -0600

    Reapply "super secret api key"
    This reverts commit 252611d623d3b25b855a59c71e26add5f6984057.

--- Objects/Classes/RaceManager.cs
+++ Objects/Classes/RaceManager.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace NeuraDrive.Objects.Classes
 {
-    class RaceManager
+    static class RaceManager
     {
         //Race Object(info the race needs)
         //List of cars racing
@@ -14,16 +15,16 @@ namespace NeuraDrive.Objects.Classes
         //generate race result(text output)
         //winning car math(assign winning car)
         //validate win lose(betting will take out amount, do not retake if lost, only check for winnings)
-        public List<Racecar> CurrentCarsRacing { get; set; }
-        public Racecar WinningCar { 
+        public static List<Racecar> CurrentCarsRacing { get; set; }
+        public static Racecar WinningCar { 
             get; 
             private set; 
         }
-        public string RaceOutput (string APIInput)
+        public static string RaceOutput (string APIInput)
         {
             return "this should be the AI commentary :3";
         }
-        private Racecar DetermineRaceWinner(List<Racecar> carsRacing)
+        private static Racecar DetermineRaceWinner(List<Racecar> carsRacing)
         {
             Random random = new Random();
             foreach(Racecar racecar in carsRacing)
