8437ac1caca4eac0f6a395b095c86131966df083
Lace-Gang <rhall@student.neumont.edu>
2025-10-22 18:41:31 -0600

    Update BettingPartial.xaml.cs

--- Objects/Partials/BettingPartial.xaml.cs
+++ Objects/Partials/BettingPartial.xaml.cs
@@ -5,9 +5,12 @@ namespace NeuraDrive.Objects.Partials;
 public partial class BettingPartial : ContentView
 {
     static RaceManager raceManager; //once we have testible data in the RaceManager itself, redo SetPartialInfo to only take in an index, and use both that and the static class to fill out all necessary info
-	public BettingPartial()
+    static User user;
+    
+    public BettingPartial()
 	{
 		InitializeComponent();
+        User.CurrentAmount = 14;
     }
 
     /// <summary>
@@ -38,7 +41,44 @@ public partial class BettingPartial : ContentView
     //The following is here until the second page is created
     private void OnBetClicked(object sender, EventArgs e)
     {
-        string betString = betAmountEntry.Text;
+        //Validate number of cars input
+        //Read text from numCarsEntry
+        //string input = numCarsEntry.Text;
+        string betInput = betAmountEntry.Text;
+
+        //Try to parse to int
+        if (int.TryParse(betInput, out int placedBet))
+        {
+            //validate number is within acceptible range
+            if (placedBet < 1 ) //Please feel free to change these values as you see fit
+            {
+                App.Current.MainPage.DisplayAlert("Invalid Input", "Cannot bet less than $1.", "OK");
+                return;
+            }
+            
+            //validate number is within acceptible range
+            if (placedBet > User.CurrentAmount) //Please feel free to change these values as you see fit
+            {
+                App.Current.MainPage.DisplayAlert("Invalid Input", "Please enter a number between 2 and 10.", "OK");
+                return;
+            }
+
+            //Run race Generation logic using numCars as number of cars to generate here:
+            //Or call a method that does it
+            //Display Race Car partials in the view (or call a method that does it)
+            App.Current.MainPage.DisplayAlert("Success!", "Your bet has been placed!", "OK");
+
+
+            //update text to show new amount of currency for the user
+
+        }
+        else
+        {
+            App.Current.MainPage.DisplayAlert("Error", "Please enter a valid number.", "OK");
+        }
+
+
+
 
         //Places a bet on a racer
         //Has to validate that the user has enough funds to place their bet
